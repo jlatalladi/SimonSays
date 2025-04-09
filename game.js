4a76d59f9a1ae42019e875da1ff484153ab5f49e
jlatalladi <112826380+jlatalladi@users.noreply.github.com>
2025-04-08 20:54:07 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,23 +1,36 @@
-let buttonColors = ["red", "blue", "green", "yellow"]; // Define four playing colors
+var buttonColors = ["red", "blue", "green", "yellow"]; // Define four playing colors
 
-let gamePattern = []; // Initialize empty pattern
-let userClickedPattern = []; // Initialized user selcted empty pattern
+var gamePattern = []; // Initialize empty pattern
+var userClickedPattern = []; // Initialized user selcted empty pattern
+
+var started = false; // Game not started
+let level = 0; // Initial level at game startup
+
+// If any key is pressed and the game is not started run nextsequence()
+$(document).keydown(function () {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
 
 $(".btn").click(function () {
-  
-  let userChosenColor = $(this).attr("id"); // Player color sequence selected
+  var userChosenColor = $(this).attr("id"); // Player color sequence selected
   userClickedPattern.push(userChosenColor); // Store player pattern
 
-  playSound(userChosenColor);               // Play sound for color selected by player
+  playSound(userChosenColor); // Play sound for color selected by player
   animatePress(userChosenColor);
 });
 
 function nextSequence() {
   // Senquece generator function
 
-  let randomNumber = Math.floor(Math.random() * 4); // Random number for index from [0 - 3]
+  level++;  // Level increase
+  $("#level-title").text("Level " + level); // Update current level to user
 
-  let randomChosenColor = buttonColors[randomNumber]; // Assing random color
+  var randomNumber = Math.floor(Math.random() * 4); // Random number for index from [0 - 3]
+
+  var randomChosenColor = buttonColors[randomNumber]; // Assing random color
 
   gamePattern.push(randomChosenColor); // Add colors to pattern
 
@@ -26,22 +39,20 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100); // Flash random color
 
-  playSound(randomChosenColor);       // Play sound for color selected random
+  playSound(randomChosenColor); // Play sound for color selected random
 }
 
 // Play random color sound
 function playSound(name) {
-  let audio = new Audio("sounds/" + name + ".mp3");
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
 // Add animation to user selected color
 function animatePress(currentColor) {
-  
-  $("." + currentColor).addClass("pressed");      // Add class for animation
+  $("." + currentColor).addClass("pressed"); // Add class for animation
 
   setTimeout(function () {
-    $("." + currentColor).removeClass("pressed"); // Remove class with timer to complete animation
+    $("." + currentColor).removeClass("pressed"); // Remove class with timer to compvare animation
   }, 100);
-  
 }
